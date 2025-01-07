96a968f67a988f74ba934f3ab96ed7209c241f98
zarus101 <83441959+zarus101@users.noreply.github.com>
2025-01-07 22:11:02 +0545

    made register responsive

--- src/pages/Userpages/LoginPage.jsx
+++ src/pages/Userpages/LoginPage.jsx
@@ -1,119 +1,97 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios, {all} from 'axios';
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(''); // Define the error state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:8000/login/', {
+      const response = await axios.post("http://127.0.0.1:8000/login/", {
         email,
         password,
       });
-      
-      localStorage.setItem('access', response.data.access);
-      localStorage.setItem('refresh', response.data.refresh);
-      localStorage.setItem('email', response.data.email);
-      localStorage.setItem('username', response.data.username);
-      let usersrole = response.data.role ;
-      let allrole = usersrole ? 'user' : "admin";
-      localStorage.setItem('role', allrole)
-      toast("Successfully Login")
-      if (allrole == 'user') {
-        navigate('/');
+
+      localStorage.setItem("access", response.data.access);
+      localStorage.setItem("refresh", response.data.refresh);
+      localStorage.setItem("email", response.data.email);
+      localStorage.setItem("username", response.data.username);
+      let usersrole = response.data.role;
+      let allrole = usersrole ? "user" : "admin";
+      localStorage.setItem("role", allrole);
+      toast("Successfully Login");
+      if (allrole === "user") {
+        navigate("/");
       } else {
-        navigate('/etrendadmin');
+        navigate("/etrendadmin");
       }
     } catch (err) {
-    setError('Email or password is invalid. '); // Set the error state
-      toast('Login failed. Please try again.');
+      setError("Email or password is invalid.");
+      toast("Login failed. Please try again.");
     }
   };
 
+  return (
+    <section className=" relative min-h-screen flex items-center justify-center bg-[url('/img/cover.png')] bg-cover bg-center bg-no-repeat p-4 sm:p-6 md:p-8 lg:p-12">
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-sky-900 opacity-70"></div>
+      <div className="z-10 w-full max-w-md space-y-8">
+        <div>
+          <Link to="/" className="flex items-center justify-center mb-6">
+            <img src="/img/logo.jpg" alt="logo" className="w-10 h-10 mr-2 rounded-full animate-bounce" />
+            <span className="text-3xl font-bold text-white">Eagle Trend</span>
+          </Link>
+          <h2 className="mt-6 text-3xl font-extrabold text-center text-white">Sign in to your Account</h2>
+        </div>
 
-    return (
-        <>
-            <section className="bg-[url('/img/cover.png')] dark:bg-gray-900 bg-cover bg-center bg-no-repeat h-screen w-full flex items-center justify-center lg:block font-poppins">
-                <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-sky-900 opacity-70"></div>
-                <div className="relative flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-full lg:py-0">
-                    <Link to="/" className="flex items-center mb-6 text-4xl font-bold text-white dark:text-transparent text-shadow-sm shadow-black">
-                        <img className="w-8 h-8 lg:mr-5 mr-3 rounded-full animate-bounce" src="/img/logo.jpg" alt="logo" />
-                        Eagle Trend
-                    </Link>
-                    <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        {error && <div className="text-red-500 text-sm">{error}</div>}
-                            <h1 className="text-xl text-center uppercase font-bold leading-tight tracking-tight text-secondary md:text-2xl dark:text-white">
-                                Sign in to your account
-                            </h1>
-                            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-secondary dark:text-white">Email</label>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        value={email}  // Bind email state
-                                        onChange={(e) => setEmail(e.target.value)} // Update email state
-                                        className="bg-gray-50 border border-s-light text-secondary rounded-lg focus:ring-secondary focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-secondary"
-                                        placeholder="name@gmail.com"
-                                        required
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-secondary dark:text-white">Password</label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
-                                        value={password}  // Bind password state
-                                        onChange={(e) => setPassword(e.target.value)} // Update password state
-                                        className="bg-gray-50 border border-gray-300 text-secondary rounded-lg focus:ring-secondary focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="••••••••"
-                                        required
-                                    />
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input
-                                                id="remember"
-                                                aria-describedby="remember"
-                                                type="checkbox"
-                                                className="w-4 h-4 border border-secondary rounded bg-gray-50 focus:ring-3 focus:ring-secondary dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-secondary dark:ring-offset-gray-800"
-                                                required
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                        </div>
-                                    </div>
-                                    <Link to="#" className="text-sm font-medium text-secondary hover:underline dark:text-primary-500">Forgot password?</Link>
-                                </div>
-                                <button
-                                    type="submit"  // Ensure the button is of type submit
-                                    className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                                >
-                                    Sign in
-                                </button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <Link to="/register" className="font-medium text-secondary hover:underline dark:text-primary-500">Sign up</Link>
-                                </p>
-                            </form>
-                           
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+        <form className="mt-8 space-y-6 bg-white shadow-md rounded-lg px-4 py-6 sm:px-8" onSubmit={handleSubmit}>
+          {error && <div className="text-red-500 text-sm text-center">{error}</div>}
+
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-secondary">
+              Email
+            </label>
+            <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full p-2.5 bg-gray-50 border border-gray-300 text-secondary rounded-lg focus:ring-secondary focus:border-primary-600" placeholder="name@gmail.com" required />
+          </div>
+          <div>
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-secondary">
+              Password
+            </label>
+            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-2.5 bg-gray-50 border border-gray-300 text-secondary rounded-lg focus:ring-secondary focus:border-primary-600" placeholder="••••••••" required />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-secondary rounded bg-gray-50 focus:ring-3 focus:ring-secondary" required />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="remember" className="text-gray-500">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <Link to="#" className="text-sm font-medium text-secondary hover:underline">
+              Forgot password?
+            </Link>
+          </div>
+          <button type="submit" className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+            Sign in
+          </button>
+          <p className="text-sm font-light text-gray-500 text-center">
+            Don't have an account yet?{" "}
+            <Link to="/register" className="font-medium text-secondary hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default LoginPage;
--- src/pages/Userpages/RegisterPage.jsx
+++ src/pages/Userpages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -43,62 +43,69 @@ const RegisterPage = () => {
         toast.warn("Registration failed. Please try again.");
       }
     } catch (err) {
-      toast.error(`Registration failed: ${err.response?.data?.message || err.message}`);
+      toast.error(`Registration failed: ${axios.isAxiosError(err) && err.response?.data?.message ? err.response.data.message : "An error occurred"}`);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <section className="bg-[url('/img/cover.png')] py-10 font-poppins flex items-center justify-center h-fit relative bg-cover bg-center">
+    <section className="min-h-screen flex items-center justify-center bg-[url('/img/cover.png')] bg-cover bg-center bg-no-repeat p-4 sm:p-6 md:p-8 lg:p-12  relative">
       <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-sky-900 opacity-70"></div>
- 
-      <div className="z-10 flex flex-col items-center w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-      <Link to="/" className="flex items-center mb-6 text-3xl font-bold text-secondary">
-        <img src="/img/logo.jpg" alt="logo" className="w-10 h-10 mr-2 rounded-full animate-bounce" />
-        Eagle Trend
-      </Link>
-        <h1 className="text-2xl font-bold text-center uppercase text-secondary">Create an Account</h1>
-        <form onSubmit={handleSubmit} className="w-full mt-6 space-y-6">
-          <div>
-            <label htmlFor="username" className="block text-sm font-medium text-secondary">
-              Username
-            </label>
-            <input type="text" name="username" id="username" value={formData.username} onChange={handleChange} required className="w-full p-2 mt-1 border rounded-lg focus:ring-secondary focus:border-secondary" />
+      <div className="z-10 w-full max-w-md space-y-8">
+        <div>
+          <Link to="/" className="flex items-center justify-center mb-6">
+            <img src="/img/logo.jpg" alt="logo" className="w-10 h-10 mr-2 rounded-full animate-bounce" />
+            <span className="text-3xl font-bold text-white">Eagle Trend</span>
+          </Link>
+          <h2 className="mt-6 text-3xl font-extrabold text-center text-white">Create an Account</h2>
+        </div>
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6 bg-white shadow-md rounded-lg px-4 py-6 sm:px-8">
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                Username
+              </label>
+              <input type="text" name="username" id="username" value={formData.username} onChange={handleChange} required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+                Confirm Password
+              </label>
+              <input type="password" name="confirmPassword" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm" />
+            </div>
           </div>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-secondary">
-              Email
-            </label>
-            <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} required className="w-full p-2 mt-1 border rounded-lg focus:ring-secondary focus:border-secondary" />
-          </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-secondary">
-              Password
-            </label>
-            <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} required className="w-full p-2 mt-1 border rounded-lg focus:ring-secondary focus:border-secondary" />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-secondary">
-              Confirm Password
-            </label>
-            <input type="password" name="confirmPassword" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required className="w-full p-2 mt-1 border rounded-lg focus:ring-secondary focus:border-secondary" />
-          </div>
-          <div className="flex items-start">
-            <input type="checkbox" id="terms" required className="w-4 h-4 mr-2" />
-            <label htmlFor="terms" className="text-sm text-secondary">
+
+          <div className="flex items-center">
+            <input type="checkbox" id="terms" required className="h-4 w-4 text-primary focus:ring-primary border-gray-300 rounded" />
+            <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">
               I accept the{" "}
-              <Link to="#" className="text-primary underline">
+              <Link to="#" className="text-primary hover:underline">
                 Terms and Conditions
               </Link>
             </label>
           </div>
-          <button type="submit" disabled={loading} className="w-full py-2 text-white bg-primary rounded-lg hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300">
-            {loading ? "Creating Account..." : "Create an Account"}
-          </button>
-          <p className="text-sm text-center text-secondary">
+
+          <div>
+            <button type="submit" disabled={loading} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+              {loading ? "Creating Account..." : "Create an Account"}
+            </button>
+          </div>
+          <p className="mt-2 text-center text-sm text-black">
             Already have an account?{" "}
-            <Link to="/login" className="text-primary underline">
+            <Link to="/login" className="font-medium text-primary hover:underline">
               Login here
             </Link>
           </p>
